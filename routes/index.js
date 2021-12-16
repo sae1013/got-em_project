49782a14e2638aaad3612c6f82384a97f6753efd
sae1013 <sae1013@gmail.com>
2021-12-17 01:36:27 +0900

    FEAT: 비밀번호 찾기 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const asyncHandler = require('../utils/async-handler');
 const hashPassword = require('../utils/hash-password');
 const sendMail = require('../utils/node-mailer');
+const generatePassword = require('../utils/generate-password');
 
 const { User } = require('../models/index');
 
@@ -39,11 +40,30 @@ router.get('/logout',(req,res)=>{
 });
 
 //비밀번호 찾기
-router.post('/find-password', asyncHandler(async (req, res) => {
-  const { email } = req.body;
-  const message = 'hello'
-  await sendMail(email, "You've received a message", message);
-  res.send("메일이 발송되었습니다.");
+router.post('/reset-password', asyncHandler(async (req, res) => {
+  const { email } = req.body; // form으로 이메일을 입력받아야함
+  const existedUser = await User.findOne({email});
+  if(!existedUser){
+    const error = new Error('가입되지않은 계정입니다');
+    error.status = 401;
+    return error
+  }
+  //유저의 비번 업데이트
+  const newPassword = generatePassword();
+  try{
+    await sendMail(email, "TEAM7: 새로운 패스워드 입니다.",`요청하신 새로운 패스워드 입니다: ${newPassword}`);//to ,subject,text
+    await User.updateOne({email},{password: hashPassword(newPassword)});
+    res.json({'success':"메일이 발송되었습니다."});
+  }catch(err){
+    const error = new Error('Server too busy')
+    error.status = 421
+    throw error
+  }
+  
 }));
+
+router.post('/find-email', asyncHandler(async(req,res)=>{
+  
+}))
 
 module.exports = router;
--- utils/node-mailer.js
+++ utils/node-mailer.js
@@ -8,12 +8,12 @@ const transport = nodemailer.createTransport({
  },
 });
 
-module.exports = (to, subject, text) => new Promise((resolve, reject) => {
+module.exports = (to, subject, content) => new Promise((resolve, reject) => {
   const message = {
     from: 'sae1013@gmail.com',
-    to:'sae1013@gmail.com',
+    to,
     subject,
-    text,
+    content,
   };
   
   transport.sendMail(message, (err, info) => {
