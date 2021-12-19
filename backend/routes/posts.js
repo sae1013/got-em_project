aa2e51d3e8892f8ecb35cde397979a4d740418d7
sae1013 <sae1013@gmail.com>
2021-12-20 01:23:26 +0900

    FIX: 상품삭제시 상품에등록된 게시글 CASCADE 작업

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,6 +7,10 @@ const loginRequired = require("../middlewares/login-required");
 router.get("/product/:productId", async (req, res) => {
   const { productId } = req.params; // product 자체를 갖고찾는거라
   const product = await Product.findOne({ shortId: productId });
+  // 해당 제품자체를 삭제한경우, 포스팅 조회도 불가해야함.
+  if(!product){
+    res.send(400).json({message:'해당제품은 삭제되었습니다'});
+  }
   const posts = await Post.find({ product });
   res.status(200).json(posts);
 });
@@ -56,12 +60,49 @@ router.patch("/write/:postId", loginRequired, async (req, res) => {
   ).populate(['product','author']);
   res.status(200).json(updatedPost);
 });
+
 // 포스팅 삭제 -> 완료
 router.delete("/:postId/delete", loginRequired, async (req, res) => {
   const { postId } = req.params;
   await Post.deleteOne({ shortId: postId });
 
   res.status(200).json({ message: "글 삭제 완료" });
+});
+
+//댓글조회 -> 미완료
+router.get("/:postId/comments", async (req, res) => {
+  const { postId } = req.params;
+  const post = await Post.findOne({ shortId: postId });
+  if (!post) {
+    res.status(400).json({ messaage: "게시글이 없습니다." });
+    return;
+  }
+  await User.populate(post.comments, { path: "author" });
+  res.status(200).json(post.comments);
+});
+
+// 댓글 추가하기 -> 미완료
+router.post("/:postId/comments/write", async (req, res) => {
+  const { postId } = req.params;
+  const { content } = req.body;
+  const author = await User.findOne({ shortId: req.user.shortId });
+  const post = Post.findOne({ shortId: postId });
+
+  if (!post) {
+    res.status(400).json({ message: "게시글이 없습니다." });
+    return;
+  }
+  // $push operator 사용하여 댓글 추가하기
+  await Post.updateOne(
+    {
+      shortId: postId,
+    },
+    {
+      $push: { comments: { content, author } },
+    }
+  );
+
+  res.status(200).json({ message: "댓글 작성 완료" });
 });
 
 module.exports = router;
