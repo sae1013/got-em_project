a3d2bd498291f32757d04f1eb8097bc147288b78
sae1013 <sae1013@gmail.com>
2021-12-20 01:16:34 +0900

    FEAT:포스팅 상세보기 완료

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -11,5 +11,19 @@ router.get("/product/:productId", async (req, res) => {
   res.status(200).json(posts);
 });
 
+// 포스팅 조회 -> 완료 
+router.get("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const post = await Post.findOneAndUpdate(
+    { shortId: postId },
+    { $inc: { viewCount: 1 } },
+    { new: true }
+  ).populate(['product','author']); 
+  if (!post) {
+    res.status(400).json({ message: "해당하는 글이 없습니다." });
+    return;
+  }
+  res.status(200).json(post);
+});
 
 module.exports = router;
