4b4c1bcbba5c16f4759df2e6570f5facd04db0d2
sae1013 <sae1013@gmail.com>
2021-12-24 16:49:11 +0900

    FEAT: 비밀번호 변경하기 추가

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const loginRequired = require('../middlewares/login-required');
 const router = express.Router();
 const { User,Product } = require('../models/index');
 const asyncHandler = require('../utils/async-handler');
+const hashPassword = require('../utils/hash-password');
 
 
 // 해당 유저가 좋아하고있는 상품들 모아보기
@@ -29,6 +30,16 @@ router.patch('/:userId/modify', asyncHandler(async(req,res)=>{
   const updatedUser = await User.findOneAndUpdate({shortId:userId},{profileUrl,nickName},{new:true});
   return res.status(200).json(updatedUser);
   
+}));
+
+router.put('/:userId/change-password',loginRequired,asyncHandler(async (req,res)=>{
+  const { userId:shortId } = req.params;
+  const { password } = req.body;
+  const user = await User.findOne({shortId});
+  user.password = hashPassword(password);
+  await user.save();
+  res.json({message:'비밀번호가 변경되었습니다'});
+
 }));
 
 module.exports = router;
